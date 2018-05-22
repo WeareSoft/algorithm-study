402ab6138ae471c6dee0e4a81a87a0da86bfb9d7
doy <ehdus5958@naver.com>
2018-05-22 16:45:21 +0900

    Update Doy_Q11652.java

--- src/doy/Doy_Q11652.java
+++ src/doy/Doy_Q11652.java
@@ -6,6 +6,11 @@ import java.util.*;
 public class Doy_Q11652 {
     // 준규는 숫자 카드 N장을 가지고 있다. 숫자 카드에는 정수가 하나 적혀있는데, 적혀있는 수는 ***-2(62)보다 크거나 같고, 2(62)보다 작거나 같다.***
     // 준규가 가지고 있는 카드가 주어졌을 때, 가장 많이 가지고 있는 정수를 구하는 프로그램을 작성하시오. 만약, 가장 많이 가지고 있는 정수가 여러가지라면, 작은 것을 출력한다.
+    /*
+    1. int : 32 bits / -2,147,483,647 ~ 2,147,483,647
+    2. long : 64 bits / -9,223,372,036,854,775,807 ~ 9,223,372,036,854,775,807
+    3. BigInteger : Immutable arbitrary-precision integers(무한대)
+    */
     public static Scanner s = new Scanner(System.in);
     public static void run() {
         int inputCount = s.nextInt();
