eefdfc41c72902b94621d3fc361b773ab3b76674
doy <ehdus5958@naver.com>
2018-05-29 18:25:22 +0900

    Update Doy_Q9613.java

--- src/doy/Doy_Q9613.java
+++ src/doy/Doy_Q9613.java
@@ -3,6 +3,8 @@ package doy;
 import java.util.Scanner;
 
 public class Doy_Q9613 {
+    // 양의 정수 n개가 주어졌을 때, 가능한 모든 쌍의 GCD의 합을 구하는 프로그램을 작성하시오.
+    // 첫째 줄에 테스트 케이스의 개수 t (1 < t < 100)이 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있고, 첫째 줄에는 수의 개수 n (1 < n < 100)가 주어지고, 다음에는 n개의 수가 주어진다. 입력으로 주어지는 수는 1000000을 넘지 않는다.
     static Scanner s = new Scanner(System.in);
     public static void run() {
         int t = s.nextInt();
