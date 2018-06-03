177b0e04885e376199cbbc30d01bba321ee851b9
Kwon HeeJeong <gmlwjd9405@naver.com>
2018-06-04 01:23:15 +0900

    Write Doy-Q2745 feedback

--- src/doy/Doy_Q2745.java
+++ src/doy/Doy_Q2745.java
@@ -36,3 +36,11 @@ public class Doy_Q2745 {
 //        System.out.println(res);
     }
 }
+
+/*
+    Integer API를 이용하면 진법 변환을 훨씬 더 간단하게 할 수 있다는 것을 알았습니다 :)
+    또한 int c = n.charAt(i); 다음과 같이 char형을 바로 int로 바꿀 때, 
+    해당 char가 숫자라면 c = c - '0'; 다음과 같이 계산해야 올바른 int가 나오는 것도 알게 되었네요^^
+    
+    앞으로 (양수의) 진수로 변환하는 문제를 풀 때는 parseInt/toString을 이용하면 좋을 거 같아요!
+*/
