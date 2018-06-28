366c86e8213dcb6ce5f4eb3ab4719d150e78335c
kwonheejeong <gmlwjd9405@naver.com>
2018-06-28 14:37:09 +0900

    Solve Q10808 by hee

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import doy.math1_2.*;
 import doy.math2_1.*;
 import doy.math2_2.*;
+import hee.datastructure1.*;
 import hee.math1_2.*;
 import hee.math2_1.*;
 import hee.math2_2.*;
@@ -89,6 +90,8 @@ public class Main {
 //        Hee_Q11689.run();
 
 //        Doy_Q10972.run();
-        Hee_Q10972.run();
+//        Hee_Q10972.run();
+
+        Hee_Q10808.run();
     }
 }
