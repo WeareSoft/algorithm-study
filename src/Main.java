24c55649980beb0acdbf388d0cbbeed2dba60ff1
kwonheejeong <gmlwjd9405@naver.com>
2018-07-01 18:34:46 +0900

    Upload some questions again

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import doy.ds1.*;
 import doy.math1_2.*;
 import doy.math2_1.*;
 import doy.math2_2.*;
+import hee.datastructure1.*;
 import hee.math1_2.*;
 import hee.math2_1.*;
 import hee.math2_2.*;
@@ -89,6 +91,11 @@ public class Main {
 //        Hee_Q11689.run();
 
 //        Doy_Q10972.run();
-        Hee_Q10972.run();
+//        Hee_Q10972.run();
+
+//        Hee_Q10828.run();
+//        Doy_Q10828.run();
+      
+        Hee_Q9012.run();
     }
 }
