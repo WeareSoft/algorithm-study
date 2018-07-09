daec22069a34c6625e0652926ee65c5e489da09e
kwonheejeong <gmlwjd9405@naver.com>
2018-07-09 14:22:24 +0900

    Add an intermediate process of solving

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import doy.math1_2.*;
 import doy.math2_1.*;
 import doy.math2_2.*;
+import hee.datastructure2.*;
 import hee.math1_2.*;
 import hee.math2_1.*;
 import hee.math2_2.*;
@@ -89,6 +90,9 @@ public class Main {
 //        Hee_Q11689.run();
 
 //        Doy_Q10972.run();
-        Hee_Q10972.run();
+//        Hee_Q10972.run();
+
+//        Hee_Q6549.run();
+        Hee_Q3015.run();
     }
 }
