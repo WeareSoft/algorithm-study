762319c67f56fe8b0d982127c46e77d71a1ddc49
kwonheejeong <gmlwjd9405@naver.com>
2018-07-14 16:54:44 +0900

    Solve Q1764 by hee

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import doy.math1_2.*;
 import doy.math2_1.*;
 import doy.math2_2.*;
 import hee.datastructure1.*;
+import hee.datastructure2.*;
 import hee.math1_2.*;
 import hee.math2_1.*;
 import hee.math2_2.*;
@@ -97,8 +98,10 @@ public class Main {
 //        Doy_Q10828.run();
       
 //        Hee_Q9012.run();
-        Doy_Q9012.run();
+//        Doy_Q9012.run();
 
 //        Doy_Q10799.run();
+
+        Hee_Q1764.run();
     }
 }
