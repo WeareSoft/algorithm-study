3b42472f193219e616f2c374311cc9f5a7b36bc9
kwonheejeong <gmlwjd9405@naver.com>
2019-01-09 20:12:33 +0900

    Solve codility lession 1 by hee

--- src/hee/codility/lesson/Iterations_BinaryGap.java
+++ src/hee/codility/lesson/Iterations_BinaryGap.java
@@ -1,7 +1,23 @@
 package hee.codility.lesson;
 
 public class Iterations_BinaryGap {
-    public static void main(String[] args) {
-        System.out.println("for test");
+    public int solution(int N) {
+        String n = Integer.toBinaryString(N);
+
+        int maxGap = 0;
+        int startIdx = 0, endIdx = 1;
+
+        while (n.length() > endIdx) {
+            if(n.charAt(endIdx) == '1') {
+                int gap = endIdx - startIdx - 1;
+                startIdx = endIdx;
+
+                if (maxGap <= gap) {
+                    maxGap = gap;
+                }
+            }
+            endIdx++;
+        }
+        return maxGap;
     }
 }
