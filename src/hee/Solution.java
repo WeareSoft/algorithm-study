feb825397850a3f7ce8221258737d3197f86229f
kwonheejeong <gmlwjd9405@naver.com>
2020-06-14 17:51:21 +0900

    hee 기본 Solution 클래스 생성

---
+++ src/hee/Solution.java
@@ -0,0 +1,12 @@
+package hee;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception{
+        solution(new int[]{1, 2, 3, 4});
+    }
+
+    public static boolean solution(int[] A) {
+        return true;
+    }
+}
---
+++ test/hee/codility/SolutionTest.java
@@ -0,0 +1,27 @@
+package hee.codility;
+
+import hee.Solution;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SolutionTest {
+
+    private Solution solution;
+
+    @Before
+    public void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    public void test() {
+        int[] A = {4, 4, 3, 0, 1, 0}; // 3
+
+        boolean result = solution.solution(A);
+
+        assertThat(result, is(true));
+    }
+}
