7c0aaa93aa50144cd99d44f33a689fe8da2a8c94
Dami <fdfdsa123@hanmail.net>
2020-06-21 19:17:18 +0900

    Solve leetcode 1 by dami

--- src/dami/leetcode/Two_Sum_1.java
+++ src/dami/leetcode/Two_Sum_1.java
@@ -1,7 +1,18 @@
 package dami.leetcode;
 
 public class Two_Sum_1 {
+
+    // int[] nums = {2, 7, 11, 15};
+    // int target = 9;
     public int[] twoSum(int[] nums, int target) {
+        for(int i=0; i<nums.length; i++) {
+            for(int j=i+1; j<nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+
         return null;
     }
 }
