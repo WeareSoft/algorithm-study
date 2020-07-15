60b5bc8acb91d9b525b7530e79963453902bbf9c
Dami <fdfdsa123@hanmail.net>
2020-07-15 20:57:17 +0900

    Modify leetcode 1, 7 by dami

--- src/dami/leetcode/Reverse_Integer_7.java
+++ src/dami/leetcode/Reverse_Integer_7.java
@@ -28,3 +28,31 @@ public class Reverse_Integer_7 {
 		}
 	}
 }
+
+/*
+class Solution {
+    public int reverse(int x) {
+
+        int result = 0;
+
+        while (x != 0) {
+
+            int pop = x % 10;
+
+            x = x / 10;
+
+            if (result > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+
+            if (result < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+
+            result = result * 10 + pop;
+        }
+
+        return result;
+    }
+}
+ */
--- src/dami/leetcode/Two_Sum_1.java
+++ src/dami/leetcode/Two_Sum_1.java
@@ -1,9 +1,8 @@
 package dami.leetcode;
 
+// Submission Detail : https://leetcode.com/submissions/detail/356520239/
 public class Two_Sum_1 {
 
-    // int[] nums = {2, 7, 11, 15};
-    // int target = 9;
     public int[] twoSum(int[] nums, int target) {
         for(int i=0; i<nums.length; i++) {
             for(int j=i+1; j<nums.length; j++) {
