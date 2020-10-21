bbeb360cc461fdcb3627645b3f71fd0923e60c71
Delf-Lee <ttjs2008@naver.com>
2020-10-21 21:44:39 +0900

    Edit programmers 42860: 조이스틱

--- src/delf/programmers/Solution42860.java
+++ src/delf/programmers/Solution42860.java
@@ -1,36 +1,54 @@
 package programmers;
 
+import org.omg.CORBA.MARSHAL;
+
+import java.util.Arrays;
+
 /**
  * 조이스틱
  * https://programmers.co.kr/learn/courses/30/lessons/42860
+ * 틀린 풀이
+ * 참고: https://keepgoing0328.tistory.com/71
  */
 public class Solution42860 {
 	public int solution(String name) {
-		int answer = -1;
-
-		for (char ch : name.toCharArray()) {
-			answer += (Math.min(ch - 'A', Math.abs('Z' - ch + 1)) + 1);
-		}
-
-		int preA = 0;
-		for (int i = 1; i < name.length(); i++) {
-			if (name.charAt(i) != 'A') {
+		char[] arr = name.toCharArray();
+		int moveCnt = 0;
+		int index = 0;
+		while (true) {
+			int inc = getInc(index, arr);
+			if (Math.abs(inc) >= arr.length) {
 				break;
 			}
-			preA++;
-		}
+			moveCnt += ((Math.min(arr[index] - 'A', Math.abs('Z' - arr[index] + 1))) + Math.abs(inc));
+			arr[index] = 'A';
+			index = inc >= 0 ? (index + inc) % arr.length : (index + inc +arr.length) % arr.length;
 
-		int postA = 0;
-		for (int i = name.length() - 1; i >= 1; i--) {
-			if (name.charAt(i) != 'A') {
+			System.out.println();
+		}
+		return moveCnt;
+	}
+
+	private int getInc(int index, char[] arr) {
+		int right;
+		for (right = 0; right < arr.length; right++) {
+			if (arr[(index + right) % arr.length] != 'A') {
 				break;
 			}
-			postA++;
 		}
-		return answer - Math.max(preA, postA);
+		int left;
+		for (left = 0; left < arr.length; left++) {
+			if (arr[(index - left + arr.length) % arr.length] != 'A') {
+				break;
+			}
+		}
+		return right < left ? right : -left;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(new Solution42860().solution("AABAAAAAAAB"));
+//		System.out.println(new Solution42860().solution("AABAAAAAAAB"));
+//		System.out.println(new Solution42860().solution("JEROEN"));
+		System.out.println(new Solution42860().solution("JAZ"));
+
 	}
 }
