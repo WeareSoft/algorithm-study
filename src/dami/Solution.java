bd394608c75ced2fc8b93f43453ad148e74cd5e8
Dami <fdfdsa123@hanmail.net>
2020-10-26 16:23:31 +0900

    Add Questions Package

--- src/dami/Solution.java
+++ src/dami/Solution.java
@@ -4,12 +4,14 @@ import dami.codingtest.CodingTest;
 import dami.hackerrank.HackerRank;
 import dami.leetcode.Leetcode;
 import dami.programmers.Programmers;
+import dami.questions.Questions;
 
 public class Solution {
     public static void main(String[] args) {
-        new Leetcode().solutions();
-        new Programmers().solutions();
-        new HackerRank().solutions();
+        //new Leetcode().solutions();
+        //new Programmers().solutions();
+        //new HackerRank().solutions();
+        new Questions().solutions();
 
         //new CodingTest().solutions();
     }
---
+++ src/dami/questions/Questions.java
@@ -0,0 +1,13 @@
+package dami.questions;
+
+public class Questions {
+	public void solutions() {
+		//new Task1().solution(6, 2, new char[] {'B', 'C'}, 2, new int[] {3, -2});
+		//new Task1().solution(17, 5, new char[] {'B', 'D', 'I', 'M', 'P'}, 11, new int[] {3,-4,5,6,-7,-8,10,-12,-15,-20,23});
+
+		//new Task2().solution(2, 6, new int[][] {{6, 2, 11, 0, 3, 5}, {6, 3, 0, 9, 0, 5}});
+		//new Task2().solution(3, 10, new int[][] {{6, 12, 0, 2, 8, 4, 0, 7, 3, 6}, {6, 1,3,0,2,8,0,0,13,8}, {6,3,0,10,6,5,7,0,0,3}});
+
+		//new Task3();
+	}
+}
