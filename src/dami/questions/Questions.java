edaf44977594fb90da1c11500d2e7f580edc94ad
Dami <fdfdsa123@hanmail.net>
2020-10-28 15:05:19 +0900

    Solve questions Task3

--- src/dami/questions/Questions.java
+++ src/dami/questions/Questions.java
@@ -8,6 +8,7 @@ public class Questions {
 		//new Task2().solution(2, 6, new int[][] {{6, 2, 11, 0, 3, 5}, {6, 3, 0, 9, 0, 5}});
 		//new Task2().solution(3, 10, new int[][] {{6, 12, 0, 2, 8, 4, 0, 7, 3, 6}, {6, 1,3,0,2,8,0,0,13,8}, {6,3,0,10,6,5,7,0,0,3}});
 
-		//new Task3();
+		//new Task3().solution(2, new String[] {"B2(RG)", "3(R2(GB))"});
+		//new Task3().solution(3, new String[] {"3(BR2(R))", "B(RGB(RG))", "1B2R3G"});
 	}
 }
