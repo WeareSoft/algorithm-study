b360918a0aabdf40d9738c47b8c7aca3dce26018
Delf-Lee <ttjs2008@naver.com>
2020-11-06 22:23:00 +0900

    Update NV06

--- src/delf/etc/NV01.java
+++ src/delf/etc/NV01.java
@@ -4,27 +4,50 @@ import java.util.*;
 
 public class NV01 {
 
-	public static void main(String[] args) {
-		String s = "/root";
-//		System.out.println(Arrays.toString(new NV01().solution(new String[]{"/", "/root", "/root/abcd", "/root/abcd/etc", "/root/abcd/hello", "/root/tmp", "/root/tmp/hello", "/root/tmp/hello/tmp"}, new String[]{})));
-//		System.out.println(Arrays.toString(new NV01().solution(new String[]{"/", "/hello", "/hello/tmp", "/root", "/root/abcd", "/root/abcd/etc", "/root/abcd/hello"}, new String[]{})));
-		System.out.println(Arrays.toString(new NV01().solution(new String[]{"/", "/hello", "/hello/tmp", "/root", "/root/abcd", "/root/abcd/etc", "/root/abcd/hello"}, new String[]{"cp /hello /root/tmp"})));
+	public static void main(String[] args) throws CloneNotSupportedException {
+		System.out.println(Arrays.toString(new NV01().solution(
+				new String[]{"/", "/hello", "/hello/tmp", "/root", "/root/abcd", "/root/abcd/etc", "/root/abcd/hello"},
+				new String[]{"mkdir /root/tmp", "cp /hello /root/tmp", "rm /hello"}))
+		);
 
+		System.out.println(Arrays.toString(new NV01().solution(
+				new String[]{"/"},
+				new String[]{"mkdir /a", "mkdir /a/b", "mkdir /a/b/c", "cp /a/b /", "rm /a/b/c"}))
+		);
 
+		System.out.println(Arrays.toString(new NV01().solution(
+				new String[]{"/"},
+				new String[]{"mkdir /a", "mkdir /a/b", "mkdir /a/b/c", "cp /a/b /", "rm /a/b/c"}))
+		);
 	}
 
-	public String[] solution(String[] directory, String[] command) {
-		Directory root = new Directory(null, directory[0]);
+	public String[] solution(String[] directory, String[] command) throws CloneNotSupportedException {
+		/* (인자로 받은) 초기 디렉토리 생성 */
+		Directory root = new Directory(null, directory[0]); // 루트 생성
 		for (int i = 1; i < directory.length; i++) {
 			mkdir(root, directory[i]);
 		}
 
-		mkdir(root, "/root/tmp");
-		cp(root, "/hello", "/root/tmp");
-		rm(root, "/hello");
+		/* 명령 수행 */
+		for (String cmd : command) {
+			String[] arg = cmd.split(" ");
+			switch (arg[0]) { // 명령어
+				case "mkdir":
+					mkdir(root, arg[1]);
+					break;
+				case "cp":
+					cp(root, arg[1], arg[2]);
+					break;
+				case "rm":
+					rm(root, arg[1]);
+					break;
+			}
+		}
 
-		List<String> result = root.tree(new ArrayList<>());
-		return result.toArray(new String[0]);
+		/* 출력 */
+		return root.tree(new ArrayList<>()).stream()
+				.sorted(String::compareTo)
+				.toArray(String[]::new);
 	}
 
 	public void mkdir(Directory root, String path) {
@@ -37,25 +60,29 @@ public class NV01 {
 		dir.add(new Directory(dir, name)); // 디렉토리 생성 및 추가
 	}
 
-	public void cp(Directory root, String sourcePath, String destPath) {
-		String[] srcPathElements = sourcePath.split("/");
-		String[] onlyPath = Arrays.copyOfRange(srcPathElements, 1, srcPathElements.length);
-		Directory sourceDir = (Directory) root.open(onlyPath);
-		Directory copiedDir = (Directory) sourceDir.remove();
+	public void cp(Directory root, String sourcePath, String destPath) throws CloneNotSupportedException {
+		String[] onlyPath = getPath(sourcePath);
+		Directory sourceDir = (Directory) root.open(onlyPath); // src 디렉토리 가져오기
 
-		String[] destPathElements = destPath.split("/");
-		onlyPath = Arrays.copyOfRange(destPathElements, 1, destPathElements.length);
-		Directory destDir = (Directory) root.open(onlyPath);
-		destDir.add(copiedDir);
-		System.out.println(copiedDir);
+		onlyPath = getPath(destPath);
+		Directory destDir = (Directory) root.open(onlyPath); // dest 디렉토리 가져오기
+
+		Directory copied = (Directory) sourceDir.clone(); // 복사
+		destDir.add(copied); // 추가
 	}
 
-	private void rm(Directory root, String path) {
-		String[] pathElements = path.split("/");
-		String[] onlyPath = Arrays.copyOfRange(pathElements, 1, pathElements.length);
-		Directory sourceDir = (Directory) root.open(onlyPath);
-		System.out.println(sourceDir);
+	public void rm(Directory root, String path) {
+		String[] onlyPath = getPath(path);
+		Directory sourceDir = (Directory) root.open(onlyPath); // 삭제할 디렉토리 가져오기
 		sourceDir.remove();
+	}
+
+	private String[] getPath(String path) { // 매핑
+		String[] pathElements = path.split("/");
+		if (pathElements.length == 0) {
+			return new String[]{};
+		}
+		return Arrays.copyOfRange(pathElements, 1, pathElements.length);
 	}
 
 
@@ -64,12 +91,13 @@ public class NV01 {
 			super(parent, name);
 		}
 
-		private Map<String, File> list;
+		private Map<String, File> list; // 하위 디렉토리(파일)
 
-		public void add(File file) {
+		public void add(File file) { // 디렉토리(파일) 추가
 			if (Objects.isNull(list)) {
 				list = new HashMap<>();
 			}
+			file.parent = this;
 			list.put(file.name, file);
 		}
 
@@ -78,24 +106,25 @@ public class NV01 {
 		}
 
 		public File remove(File file) {
-			return ((Directory) this.parent).list.remove(file.name);
+			return ((Directory) this.parent).list.remove(file.name); // 자신의 부모의 list에서 자신을 삭제
 		}
 
 		public File open(String[] path) {
-			return open(path, 0);
+			return open(path, 0); // depth: 들어간 깊이
 		}
 
 		private File open(String[] path, int depth) {
-			if (depth == path.length) {
+			if (depth == path.length) { // 목적지 도달
 				return this;
 			}
+			// 다음 디렉토리 찾아서 열기 반복
 			String name = path[depth];
 			Directory nextDir = (Directory) list.get(name);
 			return nextDir.open(path, depth + 1);
 		}
 
-		public List<String> tree(List<String> result) {
-			result.add(this.toString());
+		public List<String> tree(List<String> result) { // 배열의 형태로 출력하기 위해 List받음
+			result.add(this.toString()); // dfs 하면서 추가 (toString() 오버라이드)
 			if (Objects.isNull(list) || list.isEmpty()) {
 				return result;
 			}
@@ -103,11 +132,18 @@ public class NV01 {
 			for (String name : list.keySet()) {
 				((Directory) list.get(name)).tree(result);
 			}
-
 			return result;
 		}
 
-
+		@Override
+		protected Object clone() { // 깊은 복사를 위한 오버라이딩
+			Directory dir = new Directory(this.parent, this.name);
+			dir.list = new HashMap<>();
+			for (String s : list.keySet()) {
+				dir.list.put(s, new Directory(dir, s));
+			}
+			return dir;
+		}
 	}
 
 	class File {
@@ -117,10 +153,6 @@ public class NV01 {
 		public File(File parent, String name) {
 			this.parent = parent;
 			this.name = name;
-		}
-
-		public File getParent() {
-			return parent;
 		}
 
 		@Override
