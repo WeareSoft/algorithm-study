587dafd866d4b4883fbd795730a55002b8c93ae2
Dami <fdfdsa123@hanmail.net>
2020-12-31 19:00:00 +0900

    Solve leetcode 39. Combination Sum

--- src/dami/Solution.java
+++ src/dami/Solution.java
@@ -8,7 +8,7 @@ import dami.questions.Questions;
 
 public class Solution {
     public static void main(String[] args) {
-        //new Leetcode().solutions();
+        new Leetcode().solutions();
         //new Programmers().solutions();
         //new HackerRank().solutions();
         //new Questions().solutions();
