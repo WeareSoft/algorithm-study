8f7cad362c2bc277ce070ed0f570f135a9362676
Delf-Lee <ttjs2008@naver.com>
2021-01-17 13:29:27 +0900

    Update programmer 17677: [1차] 뉴스 클러스터링

--- src/delf/programmers/Solution17677.java
+++ src/delf/programmers/Solution17677.java
@@ -9,32 +9,28 @@ import java.util.*;
 public class Solution17677 {
 	public static void main(String[] args) {
 		System.out.println(new Solution17677().solution("FRANCE", "french"));
-//		System.out.println(new Solution17677().solution("handshake", "shake hands"));
-//		System.out.println(new Solution17677().solution("aa1+aa2", "AAAA12"));
-//		System.out.println(new Solution17677().solution("E=M*C^2", "e=m*c^2"));
-
-		// aa
-		// aa
+		System.out.println(new Solution17677().solution("handshake", "shake hands"));
+		System.out.println(new Solution17677().solution("aa1+aa2", "AAAA12"));
+		System.out.println(new Solution17677().solution("E=M*C^2", "e=m*c^2"));
 	}
 
 	private final static int TRANS = 65536;
 
 	public int solution(String str1, String str2) {
-		Map<String, Integer> list1 = generateSet(str1.toLowerCase());
-		Map<String, Integer> list2 = generateSet(str2.toLowerCase());
-
-		System.out.println(list1);
-		System.out.println(list2);
+		Map<String, Integer> list1 = generateMap(str1.toLowerCase());
+		Map<String, Integer> list2 = generateMap(str2.toLowerCase());
 
 		double intersectionElementsSize = getIntersectionElementsSize(list1, list2);
-		System.out.println(intersectionElementsSize);
 		double unionElementsSize = getUnionElementsSize(list1, list2);
-		System.out.println(unionElementsSize);
+
+		if (unionElementsSize == 0) {
+			return TRANS;
+		}
 
 		return (int) ((intersectionElementsSize / unionElementsSize) * TRANS);
 	}
 
-	private Map<String, Integer> generateSet(String s) {
+	private Map<String, Integer> generateMap(String s) { // 다중집합을 map으로 표현
 		Map<String, Integer> map = new HashMap<>();
 		for (int i = 0; i < s.length() - 1; i++) {
 			if (!Character.isLowerCase(s.charAt(i)) || !Character.isLowerCase(s.charAt(i + 1))) {
@@ -47,34 +43,39 @@ public class Solution17677 {
 	}
 
 	private int getIntersectionElementsSize(Map<String, Integer> map1, Map<String, Integer> map2) {
-		Map<String, Integer> map = new HashMap<>();
+		Map<String, Integer> result = new HashMap<>(); // 반환용
 		for (String key : map1.keySet()) {
 			if (map2.containsKey(key)) {
 				int n = Math.min(map1.get(key), map2.get(key));
-				map.put(key, n);
+				result.put(key, n);
 			}
 		}
-		System.out.println(map);
-		int cnt = 0;
 
-		for (String key : map.keySet()) {
-			cnt += map.get(key);
+		int cnt = 0;
+		for (String key : result.keySet()) { // value 카운팅
+			cnt += result.get(key);
 		}
 		return cnt;
 	}
 
 	private int getUnionElementsSize(Map<String, Integer> map1, Map<String, Integer> map2) {
-		Map<String, Integer> map = new HashMap<>();
-		for (String key : map1.keySet()) {
+		Map<String, Integer> result = new HashMap<>(map1); // 반환용(map1 카피)
+		for (String key : result.keySet()) { // map1과 map2 중 max 값
 			if (map2.containsKey(key)) {
-				int n = Math.max(map1.get(key), map2.get(key));
-				map.put(key, n);
+				int n = Math.max(result.get(key), map2.get(key));
+				result.put(key, n);
 			}
 		}
-		System.out.println(map);
+
+		for (String key : map2.keySet()) { // map1에 없는 map2 데이터 추가
+			if (!result.containsKey(key)) {
+				result.put(key, map2.get(key));
+			}
+		}
+
 		int cnt = 0;
-		for (String key : map.keySet()) {
-			cnt += map.get(key);
+		for (String key : result.keySet()) { // value 카운팅
+			cnt += result.get(key);
 		}
 		return cnt;
 	}
