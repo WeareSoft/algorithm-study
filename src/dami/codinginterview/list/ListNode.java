f7001c233df227074f2a040fc707248d20d83e9c
Dami <fdfdsa123@hanmail.net>
2021-01-21 23:10:13 +0900

    Add coding interview Array4, List1

---
+++ src/dami/codinginterview/list/ListNode.java
@@ -0,0 +1,17 @@
+package dami.codinginterview.list;
+
+public class ListNode {
+	public int val;
+	public ListNode next;
+
+	public ListNode() { }
+
+	public ListNode(int val) {
+		this.val = val;
+	}
+
+	public ListNode(int val, ListNode next) {
+		this.val = val;
+		this.next = next;
+	}
+}
