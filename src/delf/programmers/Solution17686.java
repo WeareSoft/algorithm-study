2a9de1f30ada3e70b9c5d4d509930dd93607b7eb
Delf-Lee <ttjs2008@naver.com>
2021-01-21 23:57:36 +0900

    Update programmers 17686: [3차] 파일명 정렬

--- src/delf/programmers/Solution17686.java
+++ src/delf/programmers/Solution17686.java
@@ -1,6 +1,6 @@
 package programmers;
 
-import java.util.Objects;
+import java.util.*;
 
 /**
  * [3차] 파일명 정렬
@@ -8,45 +8,66 @@ import java.util.Objects;
  */
 public class Solution17686 {
     public static void main(String[] args) {
-        System.out.println(new FileName("foo9.txt"));
-        System.out.println(new FileName("foo010bar020.zip"));
-        System.out.println(new FileName("F-15"));
-        System.out.println(new FileName("0123"));
+        System.out.println(Arrays.toString(new Solution17686().solution(new String[]{"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"})));
+        System.out.println(Arrays.toString(new Solution17686().solution(new String[]{"F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"})));
+        System.out.println(Arrays.toString(new Solution17686().solution(new String[]{"a 001.ee", "A1.zz", "a01.33"})));
     }
 
-    static class FileName {
+    public String[] solution(String[] files) {
+        String[] answer = new String[files.length];
+        FileName[] fileNames = new FileName[files.length];
+        for (int i = 0; i < fileNames.length; i++) {
+            fileNames[i] = new FileName(files[i]);
+        }
+        Arrays.sort(fileNames);
+        for (int i = 0; i < fileNames.length; i++) {
+            answer[i] = fileNames[i].origin;
+        }
+        return answer;
+    }
+
+    static class FileName implements Comparable<FileName> {
         private String origin;
         private String head;
-        private String number;
-        private String tail;
+        private int number = -1;
 
         public FileName(String origin) {
             this.origin = origin;
+
+
             for (int i = 0; i < origin.length(); i++) {
                 char ch = origin.charAt(i);
 
                 if (Objects.isNull(head)) {
                     if (Character.isDigit(ch)) {
-                        head = origin.substring(0, i);
+                        head = origin.substring(0, i).toLowerCase();
                     }
                 } else {
                     if (!Character.isDigit(ch)) {
-                        number = origin.substring(head.length(), i);
-                        tail = origin.substring(i);
+                        number = Integer.parseInt(origin.substring(head.length(), i));
                         break;
                     }
                 }
+            }
+            if (number == -1) {
+                number = Integer.parseInt(origin.substring(head.length()));
             }
         }
 
         @Override
         public String toString() {
-            return "FileName{" +
-                    "origin='" + origin + '\'' +
-                    ", head='" + head + '\'' +
-                    ", number='" + number + '\'' +
-                    ", tail='" + tail + '\'' +
-                    '}';
+            return origin;
+        }
+
+        @Override
+        public int compareTo(FileName o2) {
+            if (this.head.equals(o2.head)) {
+                if (this.number == o2.number) {
+                    return 1;
+                }
+                return Integer.compare(this.number, o2.number);
+            }
+            return this.head.compareTo(o2.head);
         }
     }
 }
