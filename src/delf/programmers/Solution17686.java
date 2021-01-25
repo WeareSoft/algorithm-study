77c73ca8a39aaa89f5fbc9a7c7b3d851e7a01774
Delf-Lee <ttjs2008@naver.com>
2021-01-25 17:26:45 +0900

    Update programmer 17686: 파일명 정렬

--- src/delf/programmers/Solution17686.java
+++ src/delf/programmers/Solution17686.java
@@ -1,73 +1,83 @@
 package programmers;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 /**
  * [3차] 파일명 정렬
  * https://programmers.co.kr/learn/courses/30/lessons/17686
  */
 public class Solution17686 {
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(new Solution17686().solution(new String[]{"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"})));
+	public static void main(String[] args) {
         System.out.println(Arrays.toString(new Solution17686().solution(new String[]{"F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"})));
         System.out.println(Arrays.toString(new Solution17686().solution(new String[]{"a 001.ee", "A1.zz", "a01.33"})));
-    }
+		System.out.println(new FileName("abc123.jpg"));
 
-    public String[] solution(String[] files) {
-        String[] answer = new String[files.length];
-        FileName[] fileNames = new FileName[files.length];
-        for (int i = 0; i < fileNames.length; i++) {
-            fileNames[i] = new FileName(files[i]);
-        }
-        Arrays.sort(fileNames);
-        for (int i = 0; i < fileNames.length; i++) {
-            answer[i] = fileNames[i].origin;
-        }
-        return answer;
-    }
+	}
 
-    static class FileName implements Comparable<FileName> {
-        private String origin;
-        private String head;
-        private int number = -1;
+	public String[] solution(String[] files) {
+		String[] answer = new String[files.length];
+		FileName[] fileNames = new FileName[files.length];
+		for (int i = 0; i < fileNames.length; i++) {
+			fileNames[i] = new FileName(files[i]);
+		}
 
-        public FileName(String origin) {
-            this.origin = origin;
+		Arrays.sort(fileNames);
 
+		for (int i = 0; i < fileNames.length; i++) {
+			answer[i] = fileNames[i].origin;
+		}
+		return answer;
+	}
 
-            for (int i = 0; i < origin.length(); i++) {
-                char ch = origin.charAt(i);
+	static class FileName implements Comparable<FileName> {
+		private String origin;
 
-                if (Objects.isNull(head)) {
-                    if (Character.isDigit(ch)) {
-                        head = origin.substring(0, i).toLowerCase();
-                    }
-                } else {
-                    if (!Character.isDigit(ch)) {
-                        number = Integer.parseInt(origin.substring(head.length(), i));
-                        break;
-                    }
-                }
-            }
-            if (number == -1) {
-                number = Integer.parseInt(origin.substring(head.length()));
-            }
-        }
+		private String head = "";
+		private int number = -1;
 
-        @Override
-        public String toString() {
-            return origin;
-        }
+		@Override
+		public String toString() {
+			return "FileName{" +
+					"origin='" + origin + '\'' +
+					", head='" + head + '\'' +
+					", number=" + number +
+					'}';
+		}
 
-        @Override
-        public int compareTo(FileName o2) {
-            if (this.head.equals(o2.head)) {
-                if (this.number == o2.number) {
-                    return 1;
-                }
-                return Integer.compare(this.number, o2.number);
-            }
-            return this.head.compareTo(o2.head);
-        }
-    }
+		public FileName(String origin) {
+			this.origin = origin;
+
+			for (int i = 0; i < origin.length(); i++) {
+				char ch = origin.charAt(i);
+
+				if (head.isEmpty()) {
+					if (Character.isDigit(ch)) {
+						head = origin.substring(0, i).toLowerCase();
+					}
+				} else {
+					if (!Character.isDigit(ch)) {
+						int endIndex = i - head.length() > 5 ? head.length() + 5 : i;
+						number = Integer.parseInt(origin.substring(head.length(), endIndex));
+						break;
+					}
+				}
+			}
+			if (number == -1) {
+				int endIndex = origin.length() - head.length() > 5 ? head.length() + 5 : origin.length();
+				number = Integer.parseInt(origin.substring(head.length(), endIndex));
+			}
+		}
+
+		@Override
+		public int compareTo(FileName o) {
+			if (this.head.equals(o.head)) {
+				if (this.number == o.number) {
+					return 1;
+				}
+				return Integer.compare(this.number, o.number);
+			}
+			return this.head.compareTo(o.head);
+		}
+	}
 }
