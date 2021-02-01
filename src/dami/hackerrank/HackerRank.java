245338df34d337342f209d83f29e41b9d71afcdb
mongzza <fdfdsa123@hanmail.net>
2021-02-01 18:45:31 +0900

    Add hackerrank New Year Chaos

--- src/dami/hackerrank/HackerRank.java
+++ src/dami/hackerrank/HackerRank.java
@@ -7,6 +7,7 @@ public class HackerRank {
 		//new The_Time_in_Words().timeInWords(12, 50);
 		//new Highest_Value_Palindrome().highestValuePalindrome("12321", 5, 3);
 		//new Sherlock_and_the_Valid_String().isValid("aabbcd");
+		//new New_Year_Chaos().minimumBribes(new int[] {1, 2, 5, 3, 7, 8, 6, 4});
 		//////////////////////////////////////////////////////////////////////////////
 	}
 }
