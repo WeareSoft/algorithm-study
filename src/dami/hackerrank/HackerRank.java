12d10c7fb6b4b98727756ac68f4d9ef4a1d8d415
mongzza <fdfdsa123@hanmail.net>
2021-02-02 10:30:40 +0900

    Add hackerrank Minimum Swaps 2

--- src/dami/hackerrank/HackerRank.java
+++ src/dami/hackerrank/HackerRank.java
@@ -8,6 +8,7 @@ public class HackerRank {
 		//new Highest_Value_Palindrome().highestValuePalindrome("12321", 5, 3);
 		//new Sherlock_and_the_Valid_String().isValid("aabbcd");
 		//new New_Year_Chaos().minimumBribes(new int[] {1, 2, 5, 3, 7, 8, 6, 4});
+		//new Minimum_Swaps_2().minimumSwaps(new int[] {4,3,1,2});
 		//////////////////////////////////////////////////////////////////////////////
 	}
 }
