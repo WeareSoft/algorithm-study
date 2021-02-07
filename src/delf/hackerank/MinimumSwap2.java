295427726eafe914851d869b1a90b2bf16399ef9
Delf-Lee <ttjs2008@naver.com>
2021-02-07 10:01:50 +0900

    Update hackerRank

--- src/delf/hackerank/MinimumSwap2.java
+++ src/delf/hackerank/MinimumSwap2.java
@@ -1,17 +1,20 @@
 package hackerank;
 
+/* Minimum Swaps 2
+ * https://www.hackerrank.com/challenges/minimum-swaps-2/problem
+ * */
 public class MinimumSwap2 {
     static int minimumSwaps(int[] arr) {
         int cnt = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != (i + 1)) {
-                int idx = arr[i];
-                int tmp = arr[idx - 1];
-                arr[idx - 1] = arr[i];
-                arr[i] = tmp;
-                cnt++;
-                i--;
+            int idx = arr[i];
+            if (idx == (i + 1)) {
+                continue;
             }
+            arr[i] = arr[idx - 1];
+            arr[idx - 1] = idx;
+            cnt++;
+            i--;
         }
         return cnt;
     }
--- src/delf/hackerank/NewYearChaos.java
+++ src/delf/hackerank/NewYearChaos.java
@@ -1,9 +1,40 @@
 package hackerank;
 
+import java.util.Arrays;
+
 /* New Year Chaos
-* https://www.hackerrank.com/challenges/new-year-chaos
-* */
+ * https://www.hackerrank.com/challenges/new-year-chaos
+ * */
 public class NewYearChaos {
+
+    static void minimumBribes2(int[] q) {
+        for (int i = 0; i < q.length; i++) {
+            if (q[i] - (i + 1) > 2) {
+                System.out.println("Too chaotic");
+                return;
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < q.length; i++) {
+            if (i + 1 == q[i]) {
+                continue;
+            }
+            int idx = i + 1;
+            for (int j = idx; j < q.length; j++) {
+                if (q[j] == idx) {
+                    int tmp = q[j - 1];
+                    q[j - 1] = q[j];
+                    q[j] = tmp;
+                    cnt++;
+                    i--;
+                    break;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+
     static void minimumBribes(int[] q) {
         int[] b = new int[q.length];
         int cnt = 0;
@@ -24,10 +55,13 @@ public class NewYearChaos {
 
 
     public static void main(String[] args) {
-        minimumBribes(new int[]{5, 1, 2, 3, 7, 8, 6, 4}); // tc
-        minimumBribes(new int[]{1, 2, 5, 3, 7, 8, 6, 4}); // 7
-        minimumBribes(new int[]{2, 1, 5, 3, 4}); // 3
-        minimumBribes(new int[]{2, 5, 1, 3, 4}); // tc
+//        minimumBribes(new int[]{1, 2, 3, 4, 5, 6, 7, 8}); // tc
+//        minimumBribes(new int[]{5, 1, 2, 3, 7, 8, 6, 4}); // tc
+        minimumBribes2(new int[]{1, 2, 5, 3, 7, 8, 6, 4}); // 7
+//        minimumBribes(new int[]{1, 2, 3, 4, 5}); // 3
+//        minimumBribes(new int[]{2, 1, 5, 3, 4}); // 3
+//        minimumBribes(new int[]{2, 1, 5, 3, 4}); // 3
+//        minimumBribes(new int[]{2, 5, 1, 3, 4}); // tc
 
 //        minimumBribes(new int[]{1, 2, 3, 4, 5});
     }
