596b244e8caafa11a954303ee5cabcbc7ed9589f
mongzza <fdfdsa123@hanmail.net>
2021-02-10 19:16:46 +0900

    Add codility MaxCounters

--- src/dami/codility/Codility.java
+++ src/dami/codility/Codility.java
@@ -1,12 +1,10 @@
 package dami.codility;
 
-import dami.codility.lesson2.CyclicRotation;
-import dami.codility.lesson2.OddOccurrencesInArray;
-
 public class Codility {
 	public void solutions() {
 		//new BinaryGap().solution(328);
 		//new CyclicRotation().solution(new int[] {3, 8, 9, 7, 6}, 3);
-		new OddOccurrencesInArray().solution(new int[] {9,3,9,3,9,7,9});
+		//new OddOccurrencesInArray().solution(new int[] {9,3,9,3,9,7,9});
+		//new MaxCounters().solution(5, new int[] {3,4,4,6,1,4,4});
 	}
 }
