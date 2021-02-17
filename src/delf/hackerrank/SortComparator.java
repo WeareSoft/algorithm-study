398725be02adcd826571886c08ee767505d4c8bf
Delf-Lee <ttjs2008@naver.com>
2021-02-17 20:47:23 +0900

    Update hacker rank: sort comparator

---
+++ src/delf/hackerrank/SortComparator.java
@@ -0,0 +1,24 @@
+package hackerrank;
+
+import java.util.Comparator;
+
+public class SortComparator {
+    static class Checker implements Comparator<Player> {
+        public int compare(Player a, Player b) {
+            if (a.score == b.score) {
+                return a.name.compareTo(b.name);
+            }
+            return b.score - a.score;
+        }
+    }
+
+    static class Player {
+        String name;
+        int score;
+
+        Player(String name, int score) {
+            this.name = name;
+            this.score = score;
+        }
+    }
+}
