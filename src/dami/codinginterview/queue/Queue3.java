654f86b8c964e5a308705458f30812bef2e5be4e
mongzza <fdfdsa123@hanmail.net>
2021-02-19 12:47:48 +0900

    Update coding interview Queue3, 4 solution

--- src/dami/codinginterview/queue/Queue3.java
+++ src/dami/codinginterview/queue/Queue3.java
@@ -6,7 +6,13 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Queue3 {
+	// 시간 : O(n)
+	// 공간 : O(B), B는 트리 한 계층의 최대 너비. 생성하는 큐에는 트리 한 계층의 노드만 들어가기 때문
 	public int levelSum(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+
 		Queue<TreeNode> nodes = new LinkedList<>();
 
 		nodes.offer(root);
@@ -31,5 +37,36 @@ public class Queue3 {
 		}
 
 		return max;
+	}
+
+	// 시간 : O(n)
+	// 공간 : O(B)
+	public int solution(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+
+		int result = root.val;
+
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.offer(root);
+
+		while (!queue.isEmpty()) {
+			int count = queue.size();
+			int sum = 0;
+
+			while (count > 0) {
+				count--;
+				TreeNode node = queue.poll();
+				sum += node.val;
+
+				if (node.left != null) queue.offer(node.left);
+				if (node.right != null) queue.offer(node.right);
+			}
+
+			result = Math.max(result, sum);
+		}
+
+		return result;
 	}
 }
