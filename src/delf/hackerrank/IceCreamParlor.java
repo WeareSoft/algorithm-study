96235d2eafe628d6b4de922fae3f8e1f5e9554ed
Delf-Lee <ttjs2008@naver.com>
2021-03-03 23:25:30 +0900

    Update hacker rank: Ice Cream Parlor

--- src/delf/hackerrank/IceCreamParlor.java
+++ src/delf/hackerrank/IceCreamParlor.java
@@ -14,23 +14,35 @@ public class IceCreamParlor {
     static void whatFlavors(int[] cost, int money) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < cost.length; i++) {
-            if(cost[i] > money) {
+            if (cost[i] > money) {
                 continue;
             }
             if (map.containsKey(cost[i])) {
-                System.out.println((map.get(cost[i]) + 1) + " " + (i + 1));
-                return;
+                if (cost[i] * 2 == money) {
+                    System.out.println((map.get(cost[i]) + 1) + " " + (i + 1));
+                    return;
+                }
             }
             map.put(cost[i], i);
         }
 
-        for (int i = 1; i < money; i++) {
+        for (int c : map.keySet()) {
+            if (map.containsKey(money - c)) {
+                int one = map.get(c) + 1;
+                int another = map.get(money - c) + 1;
+                System.out.println(Math.min(one, another) + " " + Math.max(one, another));
+                return;
+            }
+        }
+
+        /*for (int i = 1; i < money; i++) {
+            int m = map.get(i);
             if (map.containsKey(i) && map.containsKey(money - i)) {
                 int one = map.get(i) + 1;
                 int another = map.get(money - i) + 1;
                 System.out.println(Math.min(one, another) + " " + Math.max(one, another));
                 return;
             }
-        }
+        }*/
     }
 }
