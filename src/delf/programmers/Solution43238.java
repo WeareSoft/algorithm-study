66c43e43d76726e0b00503ca7abf0e14cc044c48
Delf-Lee <ttjs2008@naver.com>
2021-03-08 22:48:55 +0900

    Update programmers solution: 입국심사

--- src/delf/programmers/Solution43238.java
+++ src/delf/programmers/Solution43238.java
@@ -1,7 +1,35 @@
 package programmers;
 
+import java.util.Arrays;
+
 /**
- * @author delf
+ * 입국 심사
+ * https://programmers.co.kr/learn/courses/30/lessons/43238
  */
 public class Solution43238 {
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+
+        long left = 1;
+        long right = times[times.length - 1] * (long) n;
+        long answer = right;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long sum = 0;
+            for (long time : times) {
+                sum += (mid / time);
+            }
+
+            if (sum < n) {
+                left = mid + 1;
+            } else {
+                if (mid <= answer) {
+                    answer = mid;
+                }
+                right = mid - 1;
+            }
+        }
+        return answer;
+    }
 }
