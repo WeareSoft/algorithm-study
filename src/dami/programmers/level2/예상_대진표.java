ca85976168488da00697d5d97c256efc0f5efcec
Dami <fdfdsa123@hanmail.net>
2023-07-29 18:14:24 +0900

    Add programmers solution: 예상 대진표, 카펫

--- src/dami/programmers/level2/예상_대진표.java
+++ src/dami/programmers/level2/예상_대진표.java
@@ -17,10 +17,34 @@ public class 예상_대진표 {
 	private int divide(int a) {
 		return (a + 1) / 2;
 	}
+
+	// 1+0, 0+1 / 1+1, 0+2 / 1+2, 0+3
+	// 1 2 3 4 5 6 7 8
+	public int solution_2023(int n, int a, int b) {
+		int exponent = log2(n);
+		for (int round = 1; round <= exponent; round++) {
+			if ((a%2 + a/2) == (b%2 + b/2)) {
+				return round;
+			}
+			a = a%2 + a/2;
+			b = b%2 + b/2;
+		}
+		return 1;
+	}
+
+	private int log2(int value) {
+		return (int) (Math.log(value) / Math.log(2));
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new 예상_대진표().solution(2, 1, 2));
+	}
+
 }
 
 /*  // 다른 풀이
 
+// (a-1) XOR (b-1)값을 이진수 문자열로 변환한 길이를 세는 거니까. 예를들어 3과 7이면 11 XOR 111이고 값은 100이 되니까 문자열 길이가 바로 3라운드가 되겠네요.
 public int solution(int n, int a, int b)
 {
     return Integer.toBinaryString((a-1)^(b-1)).length();
--- src/dami/programmers/level2/카펫.java
+++ src/dami/programmers/level2/카펫.java
@@ -19,11 +19,11 @@ public class 카펫 {
 		return answer;
 	}
 
+	// 근의 공식
 	public int[] solution_wow(int brown, int red) {
 		int a = (brown+4)/2;
 		int b = red+2*a-4;
-		int[] answer = {(int)(a+Math.sqrt(a*a-4*b))/2,(int)(a-Math.sqrt(a*a-4*b))/2};
-		return answer;
+		return new int[] {(int)(a+Math.sqrt(a*a-4*b))/2,(int)(a-Math.sqrt(a*a-4*b))/2};
 	}
 
 	public static void main(String[] args) {
